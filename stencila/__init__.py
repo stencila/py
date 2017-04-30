c06ade01d686eb890f693055ebae1ba12f7f24e3
Nokome Bentley <nokome@stenci.la>
2017-04-30 09:55:14 -0700

    Add start, stop and run functions

--- stencila/__init__.py
+++ stencila/__init__.py
@@ -1,3 +1,32 @@
+import time
+
 from .value import type, pack, unpack
 from .python_context import PythonContext
 from .host import Host, host
+
+
+def start():
+    """
+    Start serving the Stencila host
+    """
+    host.start()
+
+
+def stop():
+    """
+    Stop serving the Stencila host
+    """
+    host.stop()
+
+
+def run():
+    """
+    Start serving the Stencila host
+    """
+    start()
+    print('Use Ctrl+C to stop')
+    while True:
+        try:
+            time.sleep(0x7FFFFFFF)
+        except KeyboardInterrupt:
+            break
--- stencila/__main__.py
+++ stencila/__main__.py
@@ -1,9 +1,3 @@
-import time
 import stencila
 
-stencila.host.start()
-while True:
-    try:
-        time.sleep(0x7FFFFFFF)
-    except KeyboardInterrupt:
-        break
+stencila.run()
