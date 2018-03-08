24c11401a5407569c6047e1722f93d2003de3aff
Nokome Bentley <nokome@stenci.la>
2018-03-08 16:04:49 +1300

    Refactoring of SqliteContext using new base class

--- stencila/sqlite_context.py
+++ stencila/sqlite_context.py
@@ -5,62 +5,142 @@ import string
 
 import pandas
 
-from .value import pack, unpack
-
-undefined = object()
+from .context import Context
 
 
-class SqliteContext(object):
+class SqliteContext(Context):
 
-    def __init__(self, dir=None):
-        self._dir = dir
+    def __init__(self, *args, **kwargs):
+        Context.__init__(self, *args, **kwargs)
 
         db = ':memory:'
-        if dir:
-            dbs = [file for file in os.listdir(dir) if re.match(r'^.*\.(sqlite(3?)|db(3?))$', file)]
+        if self._dir:
+            dbs = [file for file in os.listdir(self._dir) if re.match(r'^.*\.(sqlite(3?)|db(3?))$', file)]
             if len(dbs):
-                db = os.path.join(dir, dbs[0])
+                db = os.path.join(self._dir, dbs[0])
 
         self._connection = sqlite3.connect(db)
 
-    def runCode(self, code):
-        errors = None
-        output = undefined
-        try:
-            if code.lstrip().upper().startswith('SELECT '):
-                output = pandas.read_sql_query(code, self._connection)
-            else:
-                self._connection.execute(code)
-        except Exception as exc:
-            errors = [{
-                'line': 0,
-                'column': 0,
-                'message': str(exc)
-            }]
+    def compile(self, operation):
+        """
+        Compile an operation
+
+        :returns: A dictionary with ``messages`` and a compiled ``operation``
+        """
+        messages = []
+
+        type = operation.get('type')
+        if type == 'cell':
+            inputs = {}
+            outputs = {}
+
+            operation['inputs'] = inputs
+            operation['outputs'] = outputs
+        else:
+            messages.append({
+                'type': 'error',
+                'message': 'SqliteContext can not compile operations of type "%s"' % type
+            })
 
         return {
-            'errors': errors,
-            'output': None if output is undefined else pack(output)
+            'messages': messages,
+            'operation': operation
         }
 
-    def callCode(self, code, inputs={}):
-        variables = {}
-        for name, package in inputs.items():
-            value = unpack(package)
-            if isinstance(value, pandas.DataFrame):
-                value.to_sql(name, self._connection, flavor='sqlite', if_exists='replace', index=False)
-            elif type(value) == list:
-                variables[name] = tuple(value)
-            else:
-                variables[name] = value
+    def execute(self, operation):
+        compilation = self.compile(operation)
+        operation = compilation['operation']
+        messages = compilation['messages']
 
-        # Non-table inputs are available as text substitution variables
-        code_ = string.Template(code).substitute(variables)
+        value = None
 
-        return self.runCode(code_)
+        type = operation.get('type')
+        if type == 'cell':
+            # Non-table cell inputs are available as text substitution variables
+            variables = {}
+            for name, packed in operation['inputs'].items():
+                data = self.unpack(name, packed)
+                if isinstance(data, str) and str[:7] == '_table_':
+                    pass
+                elif type(data) == list:
+                    # Transform lists to tuples so that they are rendered to SQL
+                    # with parentheses instead of square brackets
+                    variables[name] = tuple(data)
+                else:
+                    variables[name] = data
+            sql = string.Template(operation['code']).substitute(variables)
+
+            if sql:
+                # If the "assign" SQL extension is used then transform the SQL
+                select = None
+                match = re.match('^\s*(\w+)\s*=\s*\b(SELECT\b.*)', sql)
+                if match:
+                    value = match.group(1)
+                    select = match.group(2)
+                elif sql.lstrip().upper().startswith('SELECT '):
+                    value = 'temp_table'
+                    select = sql
+                if select:
+                    sql = 'DROP TABLE IF EXISTS %s; CREATE TEMPORARY TABLE %s AS %s' % (value, value, select)
+
+                print((value, sql))
+
+                try:
+                    self._connection.executescript(sql)
+                except Exception as exc:
+                    messages.append({
+                        'type': 'error',
+                        'line': 0,
+                        'column': 0,
+                        'message': str(exc)
+                    })
+                else:
+                    if value:
+                        value = self.pack(value)
+        else:
+            messages.append({
+                'type': 'error',
+                'message': 'SqliteContext can not execute operation of type "%s"' % type
+            })
+
+        return {
+            'messages': messages,
+            'value': value
+        }
+
+    def pack(self, data, max_rows=30):
+        """
+        Pack data into a data package or data pointer
+
+        Currently this context only deals with tables and chooses to create
+        a package or a pointer based on the number of rows in the table
+
+        :param data: Name of the table to pack
+        """
+        rows = self._connection.execute('SELECT count(*) FROM %s' % data).fetchone()[0]
+        if rows <= max_rows:
+            data_frame = pandas.read_sql_query('SELECT * FROM %s' % data, self._connection)
+            return Context.pack(data_frame)
+        else:
+            # Return a pointer
+            return {
+                'type': 'table',
+                'host': self._host.servers['http'],
+                'context': self._name,
+                'name': data
+            }
+
+    def unpack(self, packed, name):
+        data = None
+        return data
+
+    def fetch(self, name, options={}):
+        # TODO implement options e.g. pagination
+        data_frame = pandas.read_sql_query('SELECT * FROM %s' % name, self._connection)
+        return Context.pack(data_frame)
+
 
 SqliteContext.spec = {
     'name': 'SqliteContext',
-    'base': 'Context',
-    'aliases': ['sql', 'sqlite']
+    'client': 'ContextHttpClient'
 }
--- tests/test_sqlite_context.py
+++ tests/test_sqlite_context.py
@@ -1,8 +1,9 @@
 import pandas
 import pytest
 
+from stencila.context import Context
 from stencila.sqlite_context import SqliteContext
-from stencila.value import pack
+from stencila.host import Host
 
 
 def test_new():
@@ -10,22 +11,62 @@ def test_new():
 
     assert isinstance(c, SqliteContext)
 
-
 @pytest.mark.skip(reason="wip on refactoring context API")
-def test_runCode():
+def test_execute():
     c = SqliteContext()
 
-    assert c.runCode('') == {'errors': None, 'output': None}
+    assert c.execute('') == {'messages': [], 'value': None}
 
-    assert c.runCode('SELECT 42 AS x') == {'errors': None, 'output': pack(pandas.DataFrame([{'x': 42}]))}
+    assert c.execute('SELECT 42 AS x') == {'messages': [], 'value': Context.pack(pandas.DataFrame([{'x': 42}]))}
 
-    err = c.runCode('foo')['errors'][0]
+    err = c.execute('foo')['messages'][0]
     assert err['line'] == 0
     assert err['message'] == 'near "foo": syntax error'
 
-    err = c.runCode('SELECT col FROM foo')['errors'][0]
+    err = c.execute('SELECT col FROM foo')['messages'][0]
     assert err['line'] == 0
-    assert err['message'] == "Execution failed on sql 'SELECT col FROM foo': no such table: foo"
+    assert err['message'] == 'no such table: foo'
+
+@pytest.mark.skip(reason="wip on refactoring context API")
+def test_pack():
+    host = Host()
+    host.start()
+
+    c = SqliteContext(host=host, name='sqliteContext1')
+    result = c.execute(TEST_TABLES_SQL)
+    assert result == {'messages': [], 'value': None}
+
+    # Pack to a package
+    package = c.pack('test_table_1', max_rows=256)
+    x = package['data']['data']['x']
+    assert len(x['values']) == 256
+    assert x['values'][:3] == ['a', 'a', 'a']
+
+    # Force packing to a pointer
+    pointer = c.pack('test_table_1', max_rows=0)
+    assert pointer['type'] == 'table'
+    assert pointer['host']['url'] == host.servers['http']['url']
+    assert pointer['context'] == 'sqliteContext1'
+    assert pointer['name'] == 'test_table_1'
+
+    host.stop()
+
+@pytest.mark.skip(reason="wip on refactoring context API")
+def test_fetch():
+    host = Host()
+    host.start()
+
+    context = SqliteContext(host=host, name='sqliteContext1')
+    result = context.execute(TEST_TABLES_SQL)
+    assert result == {'messages': [], 'value': None}
+
+    # Fetch a variable from context
+    package = context.fetch('test_table_1')
+    x = package['data']['data']['x']
+    assert len(x['values']) == 256
+    assert x['values'][:3] == ['a', 'a', 'a']
+
+    host.stop()
 
 
 @pytest.mark.skip(reason="wip on refactoring context API")
@@ -47,3 +88,25 @@ def test_callCode():
         'SELECT count(*) AS "count" FROM data WHERE x IN ${set}',
         {'data': pack(table), 'set': pack([2, 3, 4])}
     ) == {'errors': None, 'output': pack(pandas.DataFrame({'count': [3]}))}
+
+
+TEST_TABLES_SQL = '''
+    CREATE TABLE test_table_1 (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      x TEXT NOT NULL,
+      y INT NOT NULL
+    );
+    INSERT INTO test_table_1 (x,y)
+    SELECT 'a', 42
+    FROM (SELECT * FROM (
+         (SELECT 0 UNION ALL SELECT 1) t2,
+         (SELECT 0 UNION ALL SELECT 1) t4,
+         (SELECT 0 UNION ALL SELECT 1) t8,
+         (SELECT 0 UNION ALL SELECT 1) t16,
+         (SELECT 0 UNION ALL SELECT 1) t32,
+         (SELECT 0 UNION ALL SELECT 1) t64,
+         (SELECT 0 UNION ALL SELECT 1) t128,
+         (SELECT 0 UNION ALL SELECT 1) t256
+         )
+    );
+'''
